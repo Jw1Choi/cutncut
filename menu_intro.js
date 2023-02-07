02a30cb7ae93d81d5585ed00b2964f58bd4cd555
Choijiwoong <86772852+Jw1Choi@users.noreply.github.com>
2023-02-07 19:15:54 +0900

    fix menu margin

--- menu_intro.js
+++ menu_intro.js
@@ -10,7 +10,7 @@ template.innerHTML = `
     </a>
     <div class="menu-btn">
       <button class="menu_btn" onclick="jump_quest(7)" style="margin-top:5px;">무료체형분석</button>
-       <button class="total_menu"><img src="./hamburger.svg" style="width:17px"></button>
+       <button class="total_menu"><img src="./hamburger.svg" style="width:17px;margin-top:2px;"></button>
     </div>
 </nav>
 
