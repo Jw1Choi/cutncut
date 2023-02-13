b96ae6b1c6afe39bbfedf35c440c07fe089da430
Choijiwoong <86772852+Jw1Choi@users.noreply.github.com>
2023-02-13 19:42:59 +0900

    fix iphone video error

--- test_body_index.js
+++ test_body_index.js
@@ -2,7 +2,7 @@ const testbody = document.getElementById('test_body');
 testbody.innerHTML =` 
 <div id="wrap" >
 
-<video id="vid" muted autoplay loop  class="br40">
+<video id="vid" muted autoplay loop playsinline class="br40">
 <source id="hvid" src="https://firebasestorage.googleapis.com/v0/b/css-java-168f0.appspot.com/o/intro-video2.mp4?alt=media&token=371da673-6b49-42ab-8b3e-4c6743b7aad3" type="video/mp4" >
 </video>
 <div class="vtext">
